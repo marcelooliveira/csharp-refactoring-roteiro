dfaa75be50164c1e775dd4b040ca20729e4a3e27
marcelooliveira <mclricardo@gmail.com>
2017-10-18 15:53:04 -0200

    Split Temporary Variable
    Split Temporary Variable

---
+++ refatoracao/Program.cs
@@ -0,0 +1,14 @@
+﻿using refatoracao.R06.SplitTemporaryVariable;
+using System;
+
+namespace refatoracao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var retangulo = new Retangulo(3, 4);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ refatoracao/R06.SplitTemporaryVariable/Retangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R06.SplitTemporaryVariable
+{
+    class Retangulo
+    {
+        public Retangulo(double altura, double largura)
+        {
+            double temp = 2 * (altura + largura);
+            System.Console.WriteLine($"Perímetro: {temp}");
+            temp = altura * largura;
+            System.Console.WriteLine($"Área: {temp}");
+        }
+    }
+}
