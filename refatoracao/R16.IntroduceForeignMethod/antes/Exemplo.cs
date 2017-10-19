369daf8ca11994216a0a7567ebb0c7ea54f66bf4
marcelooliveira <mclricardo@gmail.com>
2017-10-19 18:58:00 -0200

    IntroduceForeignMethod
    IntroduceForeignMethod

---
+++ refatoracao/R16.IntroduceForeignMethod/antes/Exemplo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R16.IntroduceForeignMethod.antes
+{
+    class Exemplo
+    {
+        public Exemplo()
+        {
+            var data = DateTime.Today;
+            UltimoDiaDoMes(data);
+        }
+
+        private static void UltimoDiaDoMes(DateTime data)
+        {
+            var ultimoDiaDoMes = new DateTime(data.Year, data.Month, DateTime.DaysInMonth(data.Year, data.Month));
+        }
+    }
+}
