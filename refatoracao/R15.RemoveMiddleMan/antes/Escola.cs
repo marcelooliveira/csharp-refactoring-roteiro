c85bf14a336d7c041a356869251199e96c963c93
marcelooliveira <mclricardo@gmail.com>
2017-10-19 18:13:32 -0200

    RemoveMiddleMan
    RemoveMiddleMan

--- refatoracao/R15.RemoveMiddleMan/antes/Escola.cs
+++ refatoracao/R15.RemoveMiddleMan/antes/Escola.cs
@@ -6,5 +6,50 @@ namespace refatoracao.R15.RemoveMiddleMan.antes
 {
     class Escola
     {
+        public string Nome { get; private set; }
+        public Funcionario Diretor { get; private set; }
+    }
+
+    class Departamento
+    {
+        public Escola Escola { get; private set; }
+    }
+
+    class Funcionario
+    {
+        private Departamento Departamento { get; }
+        public Funcionario Diretor
+        {
+            get
+            {
+                return Departamento.Escola.Diretor;
+            }
+        }
+    }
+
+    class Empregado
+    {
+        private readonly Funcionario funcionario;
+        public Empregado(Funcionario funcionario)
+        {
+            this.funcionario = funcionario;
+        }
+
+        public Funcionario Diretor
+        {
+            get
+            {
+                return funcionario.Diretor;
+            }
+        }
+    }
+
+    class Teste
+    {
+        public Teste()
+        {
+            var maria = new Empregado(new Funcionario());
+            var diretorDaMaria = maria.Diretor;
+        }
     }
 }
