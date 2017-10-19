f9c69fe37835347610aa2a9f41bb5bdc1d49ac68
marcelooliveira <mclricardo@gmail.com>
2017-10-19 13:56:36 -0200

    ExtractClass
    ExtractClass

---
+++ refatoracao/R12.ExtractClass/antes/Empresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R12.ExtractClass.antes
+{
+    class Empresa
+    {
+        public string RazaoSocial { get; private set; }
+        public string CNPJ { get; private set; }
+
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string CEP { get; private set; }
+        public string Municipio { get; private set; }
+        public string UF { get; private set; }
+        public string GetEndereco()
+        {
+            return $"{Logradouro} {Numero} {Complemento} - {Bairro} - CEP {CEP} - {Municipio} - {UF}";
+        }
+    }
+}
---
+++ refatoracao/R12.ExtractClass/depois/Empresa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R12.ExtractClass.depois
+{
+    class Empresa
+    {
+        public string RazaoSocial { get; private set; }
+        public string CNPJ { get; private set; }
+        public Endereco EnderecoEntrega { get; private set; }
+        public Endereco EnderecoCobranca { get; private set; }
+    }
+
+    class Endereco
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string CEP { get; private set; }
+        public string Municipio { get; private set; }
+        public string UF { get; private set; }
+        public string GetEndereco()
+        {
+            return $"{Logradouro} {Numero} {Complemento} - {Bairro} - CEP {CEP} - {Municipio} - {UF}";
+        }
+    }
+}
