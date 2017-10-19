67010618f37ca7037640e0fa4f10dd980a4451aa
marcelooliveira <mclricardo@gmail.com>
2017-10-19 16:16:01 -0200

    HideDelegate
    HideDelegate

---
+++ refatoracao/R14.HideDelegate/depois/Escola.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R14.HideDelegate.depois
+{
+    class Escola
+    {
+        public string Nome { get; private set; }
+        public Funcionario Diretor { get; private set; }
+    }
+
+    class Departamento
+    {
+        public Escola Escola { get; private set; }
+    }
+
+    class Funcionario
+    {
+        public Departamento Departamento { get; private set; }
+        public Funcionario Diretor {
+            get
+            {
+                return Departamento.Escola.Diretor;
+            }
+        }
+    }
+
+    class Teste
+    {
+        public Teste()
+        {
+            var maria = new Funcionario();
+            var diretorDaMaria = maria.Departamento.Escola.Diretor;
+        }
+    }
+}
