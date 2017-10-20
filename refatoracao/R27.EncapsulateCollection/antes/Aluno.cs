6e60056e0e1228c5cf04a648607bd1d0c9dd91d5
marcelooliveira <mclricardo@gmail.com>
2017-10-20 17:42:04 -0200

    EncapsulateCollection
    EncapsulateCollection

---
+++ refatoracao/R27.EncapsulateCollection/antes/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R27.EncapsulateCollection.antes
+{
+    class Aluno
+    {
+        private readonly List<Curso> cursos;
+        internal List<Curso> Cursos { get => cursos; }
+
+        public Aluno()
+        {
+            cursos = new List<Curso>();
+        }
+    }
+
+    class Curso
+    {
+
+    }
+}
---
+++ refatoracao/R27.EncapsulateCollection/depois/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace refatoracao.R27.EncapsulateCollection.depois
+{
+    class Aluno
+    {
+        private readonly List<Curso> cursos;
+        public ICollection<Curso> Cursos
+        {
+            get
+            {
+                return new ReadOnlyCollection<Curso>(cursos);
+            }
+        }
+
+        public void AddCurso(Curso curso)
+        {
+            cursos.Add(curso);
+        }
+
+        public void RemoveCurso(Curso curso)
+        {
+            cursos.Remove(curso);
+        }
+
+        public Aluno()
+        {
+            cursos = new List<Curso>();
+        }
+    }
+
+    class Curso
+    {
+
+    }
+}
