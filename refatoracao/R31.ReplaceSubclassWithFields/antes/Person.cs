46c25f5128040558ba9d91f3721847fd6bf66ba3
marcelooliveira <mclricardo@gmail.com>
2017-10-20 19:41:59 -0200

    ReplaceSubclassWithFields
    ReplaceSubclassWithFields

---
+++ refatoracao/R31.ReplaceSubclassWithFields/antes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R31.ReplaceSubclassWithFields.antes
+{
+    abstract class Person
+    {
+        public abstract char GetCode();
+    }
+
+    class Male : Person
+    {
+        public override char GetCode()
+        {
+            return 'M';
+        }
+    }
+
+    class Female : Person
+    {
+        public override char GetCode()
+        {
+            return 'F';
+        }
+    }
+}
