bbf9a21f6cb244a97609b132e38f555e9a649d32
marcelooliveira <mclricardo@gmail.com>
2017-10-20 14:20:13 -0200

    ChangeValueToReference
    ChangeValueToReference

---
+++ refatoracao/R20.ChangeValueToReference/antes/Prova.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R20.ChangeValueToReference.antes
+{
+    class Prova
+    {
+        private Aluno _aluno;
+        public string NomeAluno
+        {
+            get { return _aluno.Nome;}
+            set { _aluno = new Aluno(value); }
+        }
+
+        public Prova(string nomeAluno)
+        {
+            _aluno = new Aluno(nomeAluno);
+        }
+
+        private static int NumeroDeProvasPara(IEnumerable<Prova> provas, string aluno)
+        {
+            return provas.Count(o => o.NomeAluno.Equals(aluno, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; private set; }
+        public Aluno(string nome)
+        {
+            Nome = nome;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Nome.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Nome.GetHashCode();
+        }
+    }
+}
---
+++ refatoracao/R20.ChangeValueToReference/depois/Prova.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R20.ChangeValueToReference.depois
+{
+    class Prova
+    {
+        private Aluno _aluno;
+        public string NomeAluno
+        {
+            get { return _aluno.Nome;}
+            set { _aluno = Aluno.GetAluno(value); }
+        }
+
+        public Prova(string nomeAluno)
+        {
+            _aluno = Aluno.GetAluno(nomeAluno);
+        }
+
+        private static int NumberOfOrdersFor(IEnumerable<Prova> provas, string aluno)
+        {
+            return provas.Count(o => o.NomeAluno.Equals(aluno, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+
+    class Aluno
+    {
+        private static HashSet<Aluno> alunos = new HashSet<Aluno>();
+
+        static Aluno()
+        {
+            alunos.Add(new Aluno("Chaves"));
+            alunos.Add(new Aluno("Chiquinha"));
+            alunos.Add(new Aluno("Nhonho"));
+        }
+
+        public string Nome { get; private set; }
+        private Aluno(string nome)
+        {
+            Nome = nome;
+        }
+
+        public static Aluno GetAluno(string nome)
+        {
+            return alunos.Single(c => c.Nome.Equals(nome, StringComparison.CurrentCultureIgnoreCase));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Nome.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Nome.GetHashCode();
+        }
+    }
+}
