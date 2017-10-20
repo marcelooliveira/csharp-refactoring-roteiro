4c8f0995fbe402ec81c3b5523915c917cf06d203
marcelooliveira <mclricardo@gmail.com>
2017-10-20 18:52:26 -0200

    ReplaceTypeCodeWStateStrategy
    ReplaceTypeCodeWStateStrategy

---
+++ refatoracao/R30.ReplaceTypeCodeWStateStrategy/antes/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R30.ReplaceTypeCodeWStateStrategy.antes
+{
+    class Employee
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public int Type { get; set; }
+
+        public int Salary { get; private set; }
+        public int Comission { get; private set; }
+        public int Bonus { get; private set; }
+
+        private Employee(int type)
+        {
+            Type = type;
+        }
+
+        public int GetPayAmount()
+        {
+            switch (Type)
+            {
+                case ENGINEER:
+                    return Salary;
+                case SALESMAN:
+                    return Salary + Comission;
+                case MANAGER:
+                    return Salary + Bonus;
+                default:
+                    break;
+            }
+            throw new Exception("Unknown type");
+        }
+
+        public static Employee Create(int type)
+        {
+            return new Employee(type);
+        }
+    }
+}
---
+++ refatoracao/R30.ReplaceTypeCodeWStateStrategy/depois/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R30.ReplaceTypeCodeWStateStrategy.depois
+{
+    class Employee
+    {
+        private EmployeeType Type { get; set; }
+
+        public decimal Salary { get; private set; }
+        public decimal Comission { get; private set; }
+        public decimal Bonus { get; private set; }
+
+        public Employee(EmployeeType type)
+        {
+            Type = type;
+        }
+    }
+
+    abstract class EmployeeType
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public abstract int Type { get; }
+
+        public static EmployeeType Create(int type)
+        {
+            switch (type)
+            {
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new Salesman();
+                case MANAGER:
+                    return new Manager();
+                default:
+                    break;
+            }
+            throw new Exception("Unknown type");
+        }
+        public abstract decimal GetPayAmount(Employee employee);
+    }
+
+    class Engineer : EmployeeType
+    {
+        public override int Type => ENGINEER;
+        public override decimal GetPayAmount(Employee employee)
+        {
+            return employee.Salary;
+        }
+    }
+
+    class Salesman : EmployeeType
+    {
+        public override int Type => SALESMAN;
+        public override decimal GetPayAmount(Employee employee)
+        {
+            return employee.Salary + employee.Comission;
+        }
+    }
+
+    class Manager : EmployeeType
+    {
+        public override int Type => MANAGER;
+        public override decimal GetPayAmount(Employee employee)
+        {
+            return employee.Salary + employee.Bonus;
+        }
+    }
+}
