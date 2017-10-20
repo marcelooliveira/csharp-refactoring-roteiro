3817ce71adc38fd42bd594b062d34cd85b07e2d9
marcelooliveira <mclricardo@gmail.com>
2017-10-20 18:12:07 -0200

    ReplaceTypeCodeWithSubclasses
    ReplaceTypeCodeWithSubclasses

---
+++ refatoracao/R29.ReplaceTypeCodeWithSubclasses/antes/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R29.ReplaceTypeCodeWithSubclasses.antes
+{
+    class Employee
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public int Type { get; private set; }
+
+        private Employee(int type)
+        {
+            Type = type;
+        }
+
+        public static Employee Create(int type)
+        {
+            return new Employee(type);
+        }
+    }
+}
---
+++ refatoracao/R29.ReplaceTypeCodeWithSubclasses/depois/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R29.ReplaceTypeCodeWithSubclasses.depois
+{
+    abstract class Employee
+    {
+        public const int ENGINEER = 0;
+        public const int SALESMAN = 1;
+        public const int MANAGER = 2;
+
+        public abstract int Type { get; }
+
+        public static Employee Create(int type)
+        {
+            switch (type)
+            {
+                case ENGINEER:
+                    return new Engineer();
+                case SALESMAN:
+                    return new Salesman();
+                case MANAGER:
+                    return new Manager();
+                default:
+                    break;
+            }
+            throw new Exception("Unknown type");
+        }
+    }
+
+    class Engineer : Employee
+    {
+        public override int Type => Employee.ENGINEER;
+    }
+
+    class Salesman : Employee
+    {
+        public override int Type => Employee.SALESMAN;
+    }
+
+    class Manager : Employee
+    {
+        public override int Type => Employee.MANAGER;
+    }
+}
