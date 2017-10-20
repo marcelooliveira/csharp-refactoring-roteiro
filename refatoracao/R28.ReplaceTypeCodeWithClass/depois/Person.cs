2402f4a06303838b7ea6c1ebc286466a0b5badb1
marcelooliveira <mclricardo@gmail.com>
2017-10-20 17:52:11 -0200

    ReplaceTypeCodeWithClass
    ReplaceTypeCodeWithClass

---
+++ refatoracao/R28.ReplaceTypeCodeWithClass/depois/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R28.ReplaceTypeCodeWithClass.depois
+{
+    class Person
+    {
+        public int BloodGroup { get; private set; }
+
+        public Person(int bloodGroup)
+        {
+            BloodGroup = bloodGroup;
+        }
+    }
+
+    class BloodGroup
+    {
+        public static BloodGroup O = new BloodGroup(0);
+        public static BloodGroup A = new BloodGroup(1);
+        public static BloodGroup B = new BloodGroup(2);
+        public static BloodGroup AB = new BloodGroup(3);
+
+        private int Code { get; set; }
+
+        public BloodGroup(int code)
+        {
+            Code = code;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Code.Equals((BloodGroup)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+    }
+}
