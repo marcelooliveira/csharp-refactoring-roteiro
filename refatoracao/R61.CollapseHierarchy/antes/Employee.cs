75b63eecc5f509f7d8518d69caae7e79538694a0
marcelooliveira <mclricardo@gmail.com>
2017-10-23 13:11:25 -0200

    R61.CollapseHierarchy
    R61.CollapseHierarchy

---
+++ refatoracao/R61.CollapseHierarchy/antes/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R61.CollapseHierarchy.antes
+{
+    abstract class Employee
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    class Salesman: Employee
+    {
+        public string MobileNumber { get; set; }
+    }
+}
