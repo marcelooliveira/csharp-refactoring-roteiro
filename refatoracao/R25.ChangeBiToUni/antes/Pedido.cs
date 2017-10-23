8ccda97f0ded812eef39b7b47681d056d35f68b4
marcelooliveira <mclricardo@gmail.com>
2017-10-23 17:08:41 -0200

    R25.ChangeBiToUni
    R25.ChangeBiToUni

---
+++ refatoracao/R25.ChangeBiToUni/antes/Pedido.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R25.ChangeBiToUni.depois
+{
+    class Pedido
+    {
+        private HashSet<Cliente> _clientes = new HashSet<Cliente>();
+        public IEnumerable<Cliente> Customers
+        {
+            get { return _clientes; }
+        }
+
+        public void AddCliente(Cliente cliente)
+        {
+            cliente.PedidosDeAmigos.Add(this);
+            _clientes.Add(cliente);
+        }
+
+        public void RemoveCliente(Cliente cliente)
+        {
+            cliente.PedidosDeAmigos.Remove(this);
+            _clientes.Remove(cliente);
+        }
+    }
+
+    class Cliente
+    {
+        private HashSet<Pedido> _pedidos = new HashSet<Pedido>();
+        public IEnumerable<Pedido> Pedidos
+        {
+            get { return _pedidos; }
+        }
+
+        internal HashSet<Pedido> PedidosDeAmigos
+        {
+            get { return _pedidos; }
+        }
+
+        public void AddPedido(Pedido pedido)
+        {
+            pedido.AddCliente(this);
+        }
+
+        public void RemovePedido(Pedido pedido)
+        {
+            pedido.RemoveCliente(this);
+        }
+    }
+}
