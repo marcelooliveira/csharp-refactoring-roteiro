1b0248e65a4c6d50d90e89ca469377d6a412532a
marcelooliveira <mclricardo@gmail.com>
2017-10-23 13:58:08 -0200

    R32.DecomposeConditional
    R32.DecomposeConditional

---
+++ refatoracao/R32.DecomposeConditional/antes/HotelFazenda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R32.DecomposeConditional.antes
+{
+    class HotelFazenda
+    {
+        private decimal _taxaInverno;
+        private decimal _taxaServicoInverno;
+        private decimal _taxaVerao;
+        private DateTime INICIO_VERAO = new DateTime(2017, 12, 23);
+        private DateTime FIM_VERAO = new DateTime(2018, 03, 21);
+
+        decimal GetValorTotal(DateTime data, int quantidade)
+        {
+            decimal total;
+
+            if (data.EhAntesDe(INICIO_VERAO) || data.EhDepoisDe(FIM_VERAO))
+                total = quantidade * _taxaInverno + _taxaServicoInverno;
+            else
+                total = quantidade * _taxaVerao;
+
+            return total;
+        }
+    }
+
+    static class DateTimeExtensions
+    {
+        public static bool EhAntesDe(this DateTime estaData, DateTime aquelaData)
+        {
+            return DateTime.Compare(aquelaData, estaData) < 0;
+        }
+
+        public static bool EhDepoisDe(this DateTime estaData, DateTime aquelaData)
+        {
+            return DateTime.Compare(aquelaData, estaData) > 0;
+        }
+    }
+}
---
+++ refatoracao/R32.DecomposeConditional/depois/HotelFazenda.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R32.DecomposeConditional.depois
+{
+    class HotelFazenda
+    {
+        private decimal _taxaInverno;
+        private decimal _taxaServicoInverno;
+        private decimal _taxaVerao;
+        private DateTime INICIO_VERAO = new DateTime(2017, 12, 23);
+        private DateTime FIM_VERAO = new DateTime(2018, 03, 21);
+
+        decimal GetValorTotal(DateTime data, int quantidade)
+        {
+            decimal total;
+
+            if (NaoEhVerao(data))
+                total = TaxaInverno(quantidade);
+            else
+                total = TaxaVerao(quantidade);
+
+            return total;
+        }
+
+        private decimal TaxaVerao(int quantidade)
+        {
+            return quantidade * _taxaVerao;
+        }
+
+        private decimal TaxaInverno(int quantidade)
+        {
+            return quantidade * _taxaInverno + _taxaServicoInverno;
+        }
+
+        private bool NaoEhVerao(DateTime data)
+        {
+            return data.EhAntesDe(INICIO_VERAO) || data.EhDepoisDe(FIM_VERAO);
+        }
+    }
+
+    static class DateTimeExtensions
+    {
+        public static bool EhAntesDe(this DateTime estaData, DateTime aquelaData)
+        {
+            return DateTime.Compare(aquelaData, estaData) < 0;
+        }
+
+        public static bool EhDepoisDe(this DateTime estaData, DateTime aquelaData)
+        {
+            return DateTime.Compare(aquelaData, estaData) > 0;
+        }
+    }
+}
