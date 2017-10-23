00afc8dfd701903d4095d80570bbe4ae30eb5035
marcelooliveira <mclricardo@gmail.com>
2017-10-23 15:56:16 -0200

    R24.ChangeUniToBi
    R24.ChangeUniToBi

---
+++ refatoracao/R24.ChangeUniToBi/antes/Pedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R24.ChangeUniToBi.antes
+{
+    class Pedido
+    {
+        public Cliente Cliente { get; set; }
+    }
+
+    class Cliente
+    {
+        private HashSet<Pedido> _pedidos = new HashSet<Pedido>();
+        public IEnumerable<Pedido> Pedidos
+        {
+            get { return _pedidos; }
+        }
+    }
+}
