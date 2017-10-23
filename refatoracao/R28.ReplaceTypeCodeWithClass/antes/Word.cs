ca82c88ce4b39203f2081b73f341afdf8ce0d65c
marcelooliveira <mclricardo@gmail.com>
2017-10-23 15:07:56 -0200

    R28.ReplaceTypeCodeWithClass
    R28.ReplaceTypeCodeWithClass

---
+++ refatoracao/R28.ReplaceTypeCodeWithClass/antes/Word.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R28.ReplaceTypeCodeWithClass.antes
+{
+    class Word
+    {
+        public static int Normal = 0;
+        public static int Italic = 1;
+        public static int Bold = 2;
+        public static int BoldItalic = 3;
+
+        public string Text { get; private set; }
+        public int FontStyle { get; private set; }
+
+        public Word(string text, int fontStyle)
+        {
+            Text = text;
+            FontStyle = fontStyle;
+        }
+    }
+}
---
+++ refatoracao/R28.ReplaceTypeCodeWithClass/depois/Word.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R28.ReplaceTypeCodeWithClass.depois
+{
+    class Teste
+    {
+        public Teste()
+        {
+            var word1 = new Word("Este", FontStyle.Normal);
+            var word2 = new Word("é", FontStyle.Normal);
+            var word3 = new Word("um", FontStyle.Normal);
+            var word4 = new Word("pequeno", FontStyle.Italic);
+            var word5 = new Word("teste", FontStyle.Bold);
+        }
+    }
+
+    class Word
+    {
+        public string Text { get; private set; }
+        public FontStyle FontStyle { get; private set; }
+
+        public Word(string text, int styleCode)
+        {
+            Text = text;
+            FontStyle = new FontStyle(styleCode);
+        }
+    }
+
+    class FontStyle
+    {
+        public static int Normal = 0;
+        public static int Italic = 1;
+        public static int Bold = 2;
+        public static int BoldItalic = 3;
+
+        public int StyleCode { get; private set; }
+
+        public FontStyle(int styleCode)
+        {
+            this.StyleCode = styleCode;
+        }
+    }
+}
