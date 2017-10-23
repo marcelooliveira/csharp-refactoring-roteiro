63a14fb2b6ea883963b8d8e05e76f43fc681d7a9
marcelooliveira <mclricardo@gmail.com>
2017-10-23 17:24:59 -0200

    R40.RenameMethod
    R40.RenameMethod

---
+++ refatoracao/R40.RenameMethod/antes/Retangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R40.RenameMethod.antes
+{
+    class Retangulo
+    {
+        public Retangulo(double altura, double largura)
+        {
+            System.Console.WriteLine($"Área: {GetValor(altura, largura)}");
+        }
+
+        private static double GetValor(double altura, double largura)
+        {
+            return altura * largura;
+        }
+    }
+}
