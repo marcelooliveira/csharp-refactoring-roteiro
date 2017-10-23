f88eb5a76b854c9dd8d1e0a5d4e2a744df0c9e8a
marcelooliveira <mclricardo@gmail.com>
2017-10-23 14:40:35 -0200

    R22.ReplaceArrayWithObject
    R22.ReplaceArrayWithObject

---
+++ refatoracao/R22.ReplaceArrayWithObject/antes/Empresa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R22.ReplaceArrayWithObject.antes
+{
+    class Empresa
+    {
+    }
+}
---
+++ refatoracao/R22.ReplaceArrayWithObject/depois/Empresa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R22.ReplaceArrayWithObject.depois
+{
+    class Empresa
+    {
+        public Empresa()
+        {
+            Filial filial = new Filial("Curitiba");
+            filial.SetPontos(15);
+        }
+    }
+
+    class Filial
+    {
+        private string nome;
+        private int pontos;
+
+        public Filial(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void SetPontos(int pontos)
+        {
+            this.pontos = pontos;
+        }
+    }
+}
