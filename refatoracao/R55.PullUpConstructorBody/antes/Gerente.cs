720e4e43efe4a96bfbbd0d944afc2d902a51153e
marcelooliveira <mclricardo@gmail.com>
2017-10-24 18:01:54 -0200

    Imagens R41-R64
    Imagens R41-R64

---
+++ refatoracao/R55.PullUpConstructorBody/antes/Gerente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R55.PullUpConstructorBody.antes
+{
+    class Funcionario
+    {
+        protected string _nome;
+        protected string _id;
+    }
+
+    class Gerente : Funcionario
+    {
+        private int _avaliacao;
+        public Gerente(string nome, string id, int avaliacao)
+        {
+            _nome = nome;
+            _id = id;
+            _avaliacao = avaliacao;
+        }
+    }
+
+}
---
+++ refatoracao/R55.PullUpConstructorBody/depois/Gerente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R55.PullUpConstructorBody.depois
+{
+    class Funcionario
+    {
+        protected string _nome;
+        protected string _id;
+
+        public Funcionario(string nome, string id)
+        {
+            _nome = nome;
+            _id = id;
+        }
+    }
+
+    class Gerente : Funcionario
+    {
+        private int _avaliacao;
+        public Gerente(string nome, string id, int avaliacao) : base(nome, id)
+        {
+            _avaliacao = avaliacao;
+        }
+    }
+
+}
