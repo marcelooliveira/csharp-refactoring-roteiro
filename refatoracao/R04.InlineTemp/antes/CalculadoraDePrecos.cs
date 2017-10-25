a38574c5bb82b4c7a0f89a67efa652d68223497f
marcelooliveira <mclricardo@gmail.com>
2017-10-25 19:47:38 -0200

    corrigido: InlineTemp
    corrigido: InlineTemp

---
+++ refatoracao/R04.InlineTemp/antes/CalculadoraDePrecos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R04.InlineTemp.antes
+{
+    class CalculadoraDePrecos
+    {
+        bool TemDesconto(Pedido pedido)
+        {
+            decimal valorProdutos = pedido.ValorProdutos();
+            return (valorProdutos > 1000);
+        }
+    }
+
+    class Pedido
+    {
+        private decimal valorProdutos;
+
+        public decimal ValorProdutos()
+        {
+            return valorProdutos;
+        }
+    }
+}
---
+++ refatoracao/R04.InlineTemp/depois/CalculadoraDePrecos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R04.InlineTemp.depois
+{
+    class CalculadoraDePrecos
+    {
+        bool TemDesconto(Pedido pedido)
+        {
+            return (pedido.ValorProdutos() > 1000);
+        }
+    }
+
+    class Pedido
+    {
+        private decimal valorProdutos;
+
+        public decimal ValorProdutos()
+        {
+            return valorProdutos;
+        }
+    }
+}
