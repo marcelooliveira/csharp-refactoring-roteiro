6b82279f8022423fed65b05e4eaa28fcfc3d88a0
marcelooliveira <mclricardo@gmail.com>
2017-10-26 11:16:06 -0200

    corrigido: RemoveAssignmentsToParameters
    corrigido: RemoveAssignmentsToParameters

---
+++ refatoracao/R07.RemoveAssignmentsToParameters/depois/CalculadoraDePrecos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R07.RemoveAssignmentsToParameters.depois
+{
+    class CalculadoraDePrecos
+    {
+        decimal GetDescontoFinal(decimal descontoInicial, int quantidade, int clienteHaQuantosAnos)
+        {
+            var result = descontoInicial;
+            if (descontoInicial > 50M)
+            {
+                result = 50M;
+            }
+            if (quantidade > 100)
+            {
+                result += 15M;
+            }
+            if (clienteHaQuantosAnos > 4)
+            {
+                result += 10M;
+            }
+            return result;
+        }
+    }
+}
