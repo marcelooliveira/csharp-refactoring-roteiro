e7d25d795bfd11a428e4268cad150cf023bbb584
marcelooliveira <mclricardo@gmail.com>
2017-11-16 14:45:32 -0200

    .
    .

---
+++ refatoracao/R18.SelfEncapsulateField/antes/AcidezIdeal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R18.SelfEncapsulateField.antes
+{
+    class FaixaDeAcidezIdeal
+    {
+        private double phMinimo, phMaximo;
+
+        public FaixaDeAcidezIdeal(double phMinimo, double phMaximo)
+        {
+            this.phMinimo = phMinimo;
+            this.phMaximo = phMaximo;
+        }
+
+        bool Inclui(double ph)
+        {
+            return ph >= phMinimo && ph <= phMaximo;
+        }
+    }
+}
---
+++ refatoracao/R18.SelfEncapsulateField/depois/AcidezIdeal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R18.SelfEncapsulateField.depois
+{
+    class FaixaDeAcidezIdeal
+    {
+        private double phMinimo, phMaximo;
+
+        public double PhMinimo
+        {
+            get
+            {
+                return phMinimo;
+            }
+        }
+
+        public double PhMaximo
+        {
+            get
+            {
+                return phMaximo;
+            }
+        }
+
+        public FaixaDeAcidezIdeal(double phMinimo, double phMaximo)
+        {
+            this.phMinimo = phMinimo;
+            this.phMaximo = phMaximo;
+        }
+
+        bool Inclui(double ph)
+        {
+            return ph >= PhMinimo && ph <= PhMaximo;
+        }
+    }
+}
