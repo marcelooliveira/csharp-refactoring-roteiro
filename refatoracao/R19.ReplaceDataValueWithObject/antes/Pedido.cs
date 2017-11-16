097eb1a6f31bf643d93d6fdca22b1072f909038f
marcelooliveira <mclricardo@gmail.com>
2017-11-16 15:10:08 -0200

    .
    .

---
+++ refatoracao/R19.ReplaceDataValueWithObject/antes/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R19.ReplaceDataValueWithObject.antes
+{
+    class Pedido
+    {
+        public readonly string cliente;
+        public string Cliente { get; }
+
+        public Pedido(string cliente)
+        {
+            this.cliente = cliente;
+        }
+        
+        private static int QuantidadeDePedidosDe(IEnumerable<Pedido> pedidos, string cliente)
+        {
+            return pedidos
+                .Count(p => 
+                    p.Cliente.Equals(cliente, 
+                        StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
---
+++ refatoracao/R19.ReplaceDataValueWithObject/depois/Pedido.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R19.ReplaceDataValueWithObject.depois
+{
+    class Pedido
+    {
+        public readonly Cliente cliente;
+        public Cliente Cliente { get; }
+
+        public Pedido(Cliente cliente)
+        {
+            this.cliente = cliente;
+        }
+
+        private static int QuantidadeDePedidosDe(IEnumerable<Pedido> pedidos, Cliente cliente)
+        {
+            return pedidos
+                .Count(p =>
+                    p.Cliente.Equals(cliente));
+        }
+    }
+
+    class Cliente
+    {
+        private string nome;
+
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+        }
+
+        public Cliente(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.nome.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.nome.GetHashCode();
+        }
+    }
+}
