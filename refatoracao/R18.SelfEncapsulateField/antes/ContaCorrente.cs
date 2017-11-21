eb75a11b20c210cc3ea6596a9fa4d2f21ab4414e
marcelooliveira <mclricardo@gmail.com>
2017-11-21 12:47:51 -0200

    R18 corrigido
    R18 corrigido

---
+++ refatoracao/R18.SelfEncapsulateField/antes/ContaCorrente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R18.SelfEncapsulateField.antes
+{
+    class ContaCorrente
+    {
+        public decimal saldo;
+
+        public void Sacar(decimal valor)
+        {
+            if (valor > saldo)
+            {
+                throw new ArgumentException("Saldo insuficiente");
+            }
+
+            this.saldo -= valor;
+        }
+    }
+}
---
+++ refatoracao/R18.SelfEncapsulateField/depois/ContaCorrente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R18.SelfEncapsulateField.depois
+{
+    class ContaCorrente
+    {
+        private decimal saldo;
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+
+        public ContaCorrente(decimal saldo)
+        {
+            this.saldo = saldo;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor > Saldo)
+            {
+                throw new ArgumentException("Saldo insuficiente");
+            }
+
+            this.saldo -= valor;
+        }
+    }
+}
