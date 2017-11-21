0ef7b048c5f1c88059efe4cbc94f82dedde648a9
marcelooliveira <mclricardo@gmail.com>
2017-11-21 15:07:12 -0200

    R21 corrigido
    R21 corrigido

---
+++ refatoracao/R21.ChangeReferenceToValue/antes/CartaoDeCredito.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R21.ChangeReferenceToValue.antes
+{
+    class CartaoDeCredito
+    {
+        private readonly string nome;
+        private decimal taxa;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+        }
+
+        public decimal Taxa
+        {
+            get { return taxa; }
+            set
+            {
+                taxa = value;
+            }
+        }
+
+        private CartaoDeCredito(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public static CartaoDeCredito Create(string nome)
+        {
+            return new CartaoDeCredito(nome);
+        }
+
+        public decimal ValorComTaxa(decimal valor)
+        {
+            return valor * (1.0M + Taxa);
+        }
+    }
+
+    class Exemplo
+    {
+        public CartaoDeCredito GetMasterCard()
+        {
+            return CartaoDeCredito.Create("MC");
+        }
+    }
+}
---
+++ refatoracao/R21.ChangeReferenceToValue/depois/CartaoDeCredito.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R21.ChangeReferenceToValue.depois
+{
+    class CartaoDeCredito
+    {
+        private readonly string nome;
+        private decimal taxa;
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+        }
+
+        public decimal Taxa
+        {
+            get { return taxa; }
+            set
+            {
+                taxa = value;
+            }
+        }
+
+        public CartaoDeCredito(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public decimal ValorComTaxa(decimal valor)
+        {
+            return valor * (1.0M + taxa);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var outro = obj as CartaoDeCredito;
+            if (outro == null)
+            {
+                return false;
+            }
+            return this.nome.Equals(outro.nome);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.nome.GetHashCode();
+        }
+    }
+
+    class Exemplo
+    {
+        public bool TesteMasterCard()
+        {
+            return new CartaoDeCredito("MC").Equals(new CartaoDeCredito("MC"));
+        }
+    }
+}
