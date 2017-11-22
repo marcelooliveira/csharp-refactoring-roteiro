b31a6e2c8c82317cc62dd348b3bfbeb450e978d3
marcelooliveira <mclricardo@gmail.com>
2017-11-22 13:15:54 -0200

    R31: resolvido, criei uma classe cliente para o código

---
+++ refatoracao/R31.ReplaceSubclassWithFields/antes/Paciente.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R31.ReplaceSubclassWithFields.antes
+{
+    abstract class Paciente
+    {
+        public abstract char GetSexo();
+    }
+
+    class Homem : Paciente
+    {
+        public override char GetSexo()
+        {
+            return 'M';
+        }
+    }
+
+    class Mulher : Paciente
+    {
+        public override char GetSexo()
+        {
+            return 'F';
+        }
+    }
+
+    class Hospital
+    {
+        void Teste()
+        {
+            var maria = new Mulher();
+            var jose = new Homem();
+
+            Atender(maria);
+            Atender(jose);
+        }
+
+        void Atender(Paciente paciente)
+        {
+            //codigo para atendimento hospitalar...
+        }
+    }
+}
---
+++ refatoracao/R31.ReplaceSubclassWithFields/depois/Paciente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R31.ReplaceSubclassWithFields.depois
+{
+    class Paciente
+    {
+        public char Sexo { get; private set; }
+
+        private Paciente(char sexo)
+        {
+            Sexo = sexo;
+        }
+
+        public static Paciente CriarPacienteHomem()
+        {
+            return new Paciente('M');
+        }
+
+        public static Paciente CriarPacienteMulher()
+        {
+            return new Paciente('F');
+        }
+    }
+
+    class Hospital
+    {
+        void Teste()
+        {
+            var maria = Paciente.CriarPacienteMulher();
+            var jose = Paciente.CriarPacienteHomem();
+
+            Atender(maria);
+            Atender(jose);
+        }
+
+        void Atender(Paciente paciente)
+        {
+            //codigo para atendimento hospitalar...
+        }
+    }
+}
