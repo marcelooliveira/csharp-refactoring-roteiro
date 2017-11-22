a3067b9452bedc07a2e0684d30c148e3292b08e6
marcelooliveira <mclricardo@gmail.com>
2017-11-22 15:02:47 -0200

    R33 resolvido
    R33 resolvido

---
+++ refatoracao/R33.ConsolidateConditionalExpression/antes/Segurado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R33.ConsolidateConditionalExpression.antes
+{
+    class Segurado
+    {
+        bool cumprindoCarencia;
+        public bool CumprindoCarencia
+        {
+            get => cumprindoCarencia;
+            set => cumprindoCarencia = value;
+        }
+
+        int mensalidadesAtrasadas;
+        public int MensalidadesAtrasadas
+        {
+            get => mensalidadesAtrasadas;
+            set => mensalidadesAtrasadas = value;
+        }
+
+        int mesesSemSinistro;
+        public int MesesSemSinistro
+        {
+            get => mesesSemSinistro;
+            set => mesesSemSinistro = value;
+        }
+
+        public decimal ValorSeguroAReceber()
+        {
+            if (cumprindoCarencia) return 0;
+            if (mensalidadesAtrasadas > 1) return 0;
+            if (mesesSemSinistro < 12) return 0;
+
+            decimal resultado = 0;
+
+            //
+            //Aqui é calculado o valor do seguro...
+            //
+            return resultado;
+        }
+    }
+}
---
+++ refatoracao/R33.ConsolidateConditionalExpression/depois/Segurado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R33.ConsolidateConditionalExpression.depois
+{
+    class Segurado
+    {
+        bool cumprindoCarencia;
+        public bool CumprindoCarencia
+        {
+            get => cumprindoCarencia;
+            set => cumprindoCarencia = value;
+        }
+
+        int mensalidadesAtrasadas;
+        public int MensalidadesAtrasadas
+        {
+            get => mensalidadesAtrasadas;
+            set => mensalidadesAtrasadas = value;
+        }
+
+        int mesesSemSinistro;
+        public int MesesSemSinistro
+        {
+            get => mesesSemSinistro;
+            set => mesesSemSinistro = value;
+        }
+
+        public decimal ValorSeguroAReceber()
+        {
+            if (NaoEhElegivelParaSeguro()) return 0;
+
+            decimal resultado = 0;
+
+            //
+            //Aqui é calculado o valor do seguro...
+            //
+            return resultado;
+        }
+
+        private bool NaoEhElegivelParaSeguro()
+        {
+            return cumprindoCarencia 
+                || mensalidadesAtrasadas > 1 
+                || mesesSemSinistro < 12;
+        }
+    }
+}
