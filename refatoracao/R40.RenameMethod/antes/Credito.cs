3a7ad97cd5bbd3637e12430d05101779eca4d046
marcelooliveira <mclricardo@gmail.com>
2017-12-01 13:58:40 -0200

    .
    .

---
+++ refatoracao/R40.RenameMethod/antes/Credito.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R40.RenameMethod.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            var credito = new Credito("Walter White", 10000, 9700);
+            Console.WriteLine($"Crédito disponível - Nome: {credito.NomeCliente}, " +
+                $"Valor: {credito.GetValor()}");
+        }
+    }
+
+    class Credito
+    {
+        readonly string nomeCliente;
+        public string NomeCliente => nomeCliente;
+
+        readonly decimal creditoTotal;
+        public decimal CreditoTotal => creditoTotal;
+
+        readonly decimal creditoUtilizado;
+        public decimal CreditoUtilizado => creditoUtilizado;
+
+        public Credito(string nomeCliente, decimal creditoTotal, decimal creditoUtilizado)
+        {
+            this.nomeCliente = nomeCliente;
+            this.creditoTotal = creditoTotal;
+            this.creditoUtilizado = creditoUtilizado;
+        }
+
+        public decimal GetValor()
+        {
+            return this.creditoTotal - this.creditoUtilizado;
+        }
+    }
+}
