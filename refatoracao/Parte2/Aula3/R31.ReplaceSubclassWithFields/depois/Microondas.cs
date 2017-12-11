f3869b4d8a96cec8cf5967b3c3444782da88eb98
marcelooliveira <mclricardo@gmail.com>
2017-12-11 12:13:38 -0200

    .

---
+++ refatoracao/Parte2/Aula3/R31.ReplaceSubclassWithFields/depois/Microondas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R31.ReplaceSubclassWithFields.depois
+{
+    class Programa
+    {
+        void Main()
+        {
+            var fabrica = new Fabrica();
+            fabrica.Fabricar();
+        }
+    }
+
+    public class Microondas
+    {
+        private readonly int voltagem;
+
+        private Microondas(int voltagem)
+        {
+            this.voltagem = voltagem;
+        }
+
+        public int GetVoltagem()
+        {
+            return voltagem;
+        }
+
+        public static Microondas CriarVersao110Volts()
+        {
+            return new Microondas(110);
+        }
+
+        public static Microondas CriarVersao220Volts()
+        {
+            return new Microondas(220);
+        }
+    }
+
+    class Fabrica
+    {
+        public void Fabricar()
+        {
+            var aparelho1 = Microondas.CriarVersao110Volts();
+            var aparelho2 = Microondas.CriarVersao220Volts();
+
+            Fabricar(aparelho1);
+            Fabricar(aparelho2);
+        }
+
+        public void Fabricar(Microondas microondas)
+        {
+            //código para fabricar equipamento...
+        }
+    }
+}
