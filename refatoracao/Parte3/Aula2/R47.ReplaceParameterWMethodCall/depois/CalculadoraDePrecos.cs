4061d22732c95b61bd86046be24b5557c3f50346
marcelooliveira <mclricardo@gmail.com>
2017-12-12 16:31:44 -0200

    .
    .

---
+++ refatoracao/Parte3/Aula2/R47.ReplaceParameterWMethodCall/depois/CalculadoraDePrecos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R47.ReplaceParameterWMethodCall.depois
+{
+    class Program
+    {
+        void Main(decimal descontoInicial, int quantidade, string cpfCliente)
+        {
+            var descontoCliente =
+                new CalculadoraDePrecos()
+                .GetDescontoFinal(descontoInicial, quantidade, cpfCliente);
+
+            Console.WriteLine($"Desconto final: {descontoCliente}");
+        }
+    }
+
+    class CalculadoraDePrecos
+    {
+        private const decimal LIMITE_MAXIMO_DESCONTO_INICIAL = 50m;
+        private const int LIMITE_MINIMO_QUANTIDADE = 100;
+        private const int LIMITE_MINIMO_ANOS_CLIENTE = 4;
+        private const decimal DESCONTO_MAXIMO = 50m;
+        private const decimal INCREMENTO_DESCONTO_POR_QUANTIDADE = 15m;
+        private const decimal INCREMENTO_DESCONTO_POR_TEMPO = 10m;
+
+        public decimal GetDescontoFinal(decimal descontoInicial, int quantidade, string cpfCliente)
+        {
+            int clienteHaQuantosAnos = ServicoDeCredito.ClienteHaQuantosAnos(cpfCliente);
+            bool clienteNegativado = ServicoDeCredito.VerificaClienteNegativado(cpfCliente);
+
+            if (clienteNegativado)
+            {
+                return 0; //early return
+            }
+
+            var result = descontoInicial;
+            if (descontoInicial > LIMITE_MAXIMO_DESCONTO_INICIAL)
+            {
+                result = DESCONTO_MAXIMO;
+            }
+            if (quantidade > LIMITE_MINIMO_QUANTIDADE)
+            {
+                result += INCREMENTO_DESCONTO_POR_QUANTIDADE;
+            }
+            if (clienteHaQuantosAnos > LIMITE_MINIMO_ANOS_CLIENTE)
+            {
+                result += INCREMENTO_DESCONTO_POR_TEMPO;
+            }
+            return result;
+        }
+    }
+
+    class ServicoDeCredito
+    {
+        internal static int ClienteHaQuantosAnos(string cpfCliente)
+        {
+            return 5;
+        }
+
+        internal static bool VerificaClienteNegativado(string cpfCliente)
+        {
+            return false;
+        }
+    }
+}
