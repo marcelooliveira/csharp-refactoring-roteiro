df74c26c14b2b259498af5cbddc773064cc6229a
marcelooliveira <mclricardo@gmail.com>
2017-12-12 18:38:06 -0200

    .

---
+++ refatoracao/Parte3/Aula3/R51.ReplaceConstructorWithFactoryMethod/antes/Funcionario.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R51.ReplaceConstructorWithFactoryMethod.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            Funcionario engenheiro = new Funcionario(Funcionario.TipoFuncionario.Engenheiro, "José da Silva", 1000);
+            Funcionario vendedor = new Funcionario(Funcionario.TipoFuncionario.Vendedor, "Maria Bonita", 2000);
+            Funcionario gerente = new Funcionario(Funcionario.TipoFuncionario.Gerente, "João das Neves", 3000);
+
+            var valorFolhaDePagamento = 
+                engenheiro.Salario 
+                + vendedor.Salario 
+                + gerente.Salario;
+        }
+    }
+
+    class Funcionario
+    {
+        public enum TipoFuncionario
+        {
+            Engenheiro = 0,
+            Vendedor = 1,
+            Gerente = 2
+        }
+
+        readonly TipoFuncionario tipo;
+        public TipoFuncionario Tipo { get; }
+
+        readonly string nome;
+        public string Nome => nome;
+
+        readonly decimal salario;
+        public decimal Salario => salario;
+
+        public Funcionario(TipoFuncionario tipo, string nome, decimal salario)
+        {
+            this.tipo = tipo;
+            this.nome = nome;
+            this.salario = salario;
+        }
+    }
+}
---
+++ refatoracao/Parte3/Aula3/R51.ReplaceConstructorWithFactoryMethod/depois/Funcionario.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R51.ReplaceConstructorWithFactoryMethod.depois
+{
+    class Programa
+    {
+        void Main()
+        {
+            Funcionario engenheiro = Funcionario.CriarFuncionario(Funcionario.TipoFuncionario.Engenheiro, "José da Silva", 1000);
+            Funcionario vendedor = Funcionario.CriarFuncionario(Funcionario.TipoFuncionario.Vendedor, "Maria Bonita", 2000);
+            Funcionario gerente = Funcionario.CriarFuncionario(Funcionario.TipoFuncionario.Gerente, "João das Neves", 3000);
+
+            var valorFolhaDePagamento =
+                engenheiro.Salario
+                + vendedor.Salario
+                + gerente.Salario;
+        }
+    }
+
+    class Funcionario
+    {
+        public enum TipoFuncionario
+        {
+            Engenheiro = 0,
+            Vendedor = 1,
+            Gerente = 2
+        }
+
+        protected TipoFuncionario tipo;
+        public TipoFuncionario Tipo { get; }
+
+        protected string nome;
+        public string Nome => nome;
+
+        protected decimal salario;
+        public decimal Salario => salario;
+
+        protected Funcionario(string nome, decimal salario)
+        {
+            this.nome = nome;
+            this.salario = salario;
+        }
+
+        public static Funcionario CriarFuncionario(TipoFuncionario tipo, string nome, decimal salario)
+        {
+            switch (tipo)
+            {
+                case TipoFuncionario.Engenheiro:
+                    return new Engenheiro(nome, salario);
+                case TipoFuncionario.Vendedor:
+                    return new Vendedor(nome, salario);
+                case TipoFuncionario.Gerente:
+                    return new Gerente(nome, salario);
+                default:
+                    throw new ArgumentException("Tipo de funcionário inválido");
+            }
+        }
+    }
+
+    class Engenheiro : Funcionario
+    {
+        public Engenheiro(string nome, decimal salario) : base(nome, salario)
+        {
+            this.tipo = TipoFuncionario.Engenheiro;
+        }
+    }
+
+    class Vendedor : Funcionario
+    {
+        public Vendedor(string nome, decimal salario) : base(nome, salario)
+        {
+            this.tipo = TipoFuncionario.Vendedor;
+        }
+    }
+
+    class Gerente : Funcionario
+    {
+        public Gerente(string nome, decimal salario) : base(nome, salario)
+        {
+            this.tipo = TipoFuncionario.Gerente;
+        }
+    }
+
+}
