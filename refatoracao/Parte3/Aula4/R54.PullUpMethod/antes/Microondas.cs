5a61dbfc019b7990f35e52d4c0789fb02e2d7dc0
marcelooliveira <mclricardo@gmail.com>
2017-12-13 13:56:59 -0200

    .

---
+++ refatoracao/Parte3/Aula4/R54.PullUpMethod/antes/Microondas.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R54.PullUpMethod.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            var fabrica = new Fabrica();
+            fabrica.Fabricar();
+        }
+    }
+
+    abstract class Microondas
+    {
+        protected double altura;
+        protected double largura;
+        protected double profundidade;
+
+        public Microondas(double altura, double largura, double profundidade)
+        {
+            this.altura = altura;
+            this.largura = largura;
+            this.profundidade = profundidade;
+        }
+
+        public abstract int GetVoltagem();
+    }
+
+    class Microondas110 : Microondas
+    {
+        public Microondas110(double altura, double largura, double profundidade) : base(altura, largura, profundidade)
+        {
+        }
+
+        public override int GetVoltagem()
+        {
+            return 110;
+        }
+
+        public double GetVolume()
+        {
+            return altura * largura * profundidade;
+        }
+    }
+
+    class Microondas220 : Microondas
+    {
+        public Microondas220(double altura, double largura, double profundidade) : base(altura, largura, profundidade)
+        {
+        }
+
+        public override int GetVoltagem()
+        {
+            return 220;
+        }
+
+        public double GetVolume()
+        {
+            return altura * largura * profundidade;
+        }
+    }
+
+    class Fabrica
+    {
+        public void Fabricar()
+        {
+            var aparelho1 = new Microondas220(28.9, 43.1, 34.1);
+            var aparelho2 = new Microondas110(28.9, 43.1, 34.1);
+
+            Fabricar(aparelho1);
+            Fabricar(aparelho2);
+        }
+
+        public void Fabricar(Microondas microondas)
+        {
+            //código para fabricar equipamento...
+        }
+    }
+}
