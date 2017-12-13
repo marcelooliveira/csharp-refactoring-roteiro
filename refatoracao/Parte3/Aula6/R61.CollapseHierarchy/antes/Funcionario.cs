3a3b72d3fe53d26b16fadadcd58d9de6caad4106
marcelooliveira <mclricardo@gmail.com>
2017-12-13 16:44:54 -0200

    .

---
+++ refatoracao/Parte3/Aula6/R61.CollapseHierarchy/antes/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R61.CollapseHierarchy.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            var vendedor = new Vendedor("Walter White", "555-12345", "666-65432");
+        }
+    }
+
+    abstract class Funcionario
+    {
+        public string Nome { get; set; }
+        public string TelefoneFixo { get; set; }
+        public string Celular { get; set; }
+
+        public Funcionario(string nome, string telefoneFixo, string celular)
+        {
+            Nome = nome;
+            TelefoneFixo = telefoneFixo;
+            Celular = celular;
+        }
+    }
+
+    class Vendedor : Funcionario
+    {
+        public Vendedor(string nome, string telefoneFixo, string celular) : base(nome, telefoneFixo, celular)
+        {
+        }
+    }
+}
