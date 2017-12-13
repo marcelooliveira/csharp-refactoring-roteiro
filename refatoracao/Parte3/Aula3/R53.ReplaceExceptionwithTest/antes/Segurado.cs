d8fb6c4657de1a9c989eb32348e9072f701b0929
marcelooliveira <mclricardo@gmail.com>
2017-12-13 13:30:44 -0200

    .

---
+++ refatoracao/Parte3/Aula3/R53.ReplaceExceptionwithTest/antes/Segurado.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R53.ReplaceExceptionwithTest.antes
+{
+    class Programa
+    {
+        void Main()
+        {
+            var segurado = new Segurado();
+            segurado.CumprindoCarencia = true;
+            segurado.MensalidadesAtrasadas = 2;
+            segurado.MesesSemSinistro = 3;
+
+            decimal seguroAReceber = 0;
+
+            try
+            {
+                segurado.ValorSeguroAReceber();
+            }
+            catch 
+            {
+            }
+
+            Console.WriteLine($"Valor do seguro: {seguroAReceber}");
+        }
+    }
+
+    class Segurado
+    {
+        bool cumprindoCarencia;
+        public bool CumprindoCarencia
+        {
+            get => cumprindoCarencia;
+            set => cumprindoCarencia = value;
+        }
+
+        int mensalidadesAtrasadas;
+        public int MensalidadesAtrasadas
+        {
+            get => mensalidadesAtrasadas;
+            set => mensalidadesAtrasadas = value;
+        }
+
+        int mesesSemSinistro;
+        public int MesesSemSinistro
+        {
+            get => mesesSemSinistro;
+            set => mesesSemSinistro = value;
+        }
+
+        public decimal ValorSeguroAReceber()
+        {
+            if (NaoEhElegivelParaSeguro())
+            {
+                throw new Exception("Segurado não é elegível para receber seguro");
+            }
+
+            decimal resultado = 0;
+
+            //
+            //Aqui é calculado o valor do seguro...
+            //
+            return resultado;
+        }
+
+        private bool NaoEhElegivelParaSeguro()
+        {
+            return cumprindoCarencia 
+                || mensalidadesAtrasadas > 1 
+                || mesesSemSinistro < 12;
+        }
+    }
+}
