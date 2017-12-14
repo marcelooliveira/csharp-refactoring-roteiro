13b80c8820a3e4c29f00b6267668612033b8be88
marcelooliveira <mclricardo@gmail.com>
2017-12-14 15:25:14 -0200

    .

---
+++ refatoracao/Parte3/Aula6/R64.ReplaceDelegationWithInheritance/depois/Imovel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.Parte3.Aula6.R64.ReplaceDelegationWithInheritance.depois
+{
+    class Programa
+    {
+        void Teste()
+        {
+            var imovel = 
+                new Apartamento("Rua dos Bobos, No. 0", 100000, 200);
+        }
+    }
+
+    class Imovel
+    {
+        private readonly String endereco;
+        private decimal valorImovel;
+
+        public string Endereco => endereco;
+        public decimal Valor { get => valorImovel; set => valorImovel = value; }
+
+        public Imovel(string endereco, decimal valorImovel)
+        {
+            this.endereco = endereco;
+            this.valorImovel = valorImovel;
+        }
+    }
+
+    class Apartamento : Imovel
+    {
+        private decimal valorCondominio;
+
+        public Apartamento(string endereco, decimal valorImovel, decimal valorCondominio) : base(endereco, valorImovel)
+        {
+            this.valorCondominio = valorCondominio;
+        }
+    }
+}
