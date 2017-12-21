b46e6e5627c1383e7ee1d9a7daf9f4336c347dce
marcelooliveira <mclricardo@gmail.com>
2017-12-21 14:35:40 -0200

    .

--- refatoracao/Parte3/Aula3/R51.ReplaceConstructorWithFactoryMethod/antes/Funcionario.cs
+++ refatoracao/Parte3/Aula3/R51.ReplaceConstructorWithFactoryMethod/antes/Funcionario.cs
@@ -8,40 +8,41 @@ namespace refatoracao.R51.ReplaceConstructorWithFactoryMethod.antes
     {
         void Main()
         {
-            Funcionario engenheiro = new Funcionario(Funcionario.TipoFuncionario.Engenheiro, "José da Silva", 1000);
-            Funcionario vendedor = new Funcionario(Funcionario.TipoFuncionario.Vendedor, "Maria Bonita", 2000);
-            Funcionario gerente = new Funcionario(Funcionario.TipoFuncionario.Gerente, "João das Neves", 3000);
-
-            var valorFolhaDePagamento = 
-                engenheiro.Salario 
-                + vendedor.Salario 
-                + gerente.Salario;
+            Funcionario funcionario = new Funcionario("José da Silva", 1000);
         }
     }
 
     class Funcionario
     {
-        public enum TipoFuncionario
-        {
-            Engenheiro = 0,
-            Vendedor = 1,
-            Gerente = 2
-        }
-
-        readonly TipoFuncionario tipo;
-        public TipoFuncionario Tipo { get; }
-
         readonly string nome;
         public string Nome => nome;
 
         readonly decimal salario;
         public decimal Salario => salario;
 
-        public Funcionario(TipoFuncionario tipo, string nome, decimal salario)
+        public Funcionario(string nome, decimal salario)
         {
-            this.tipo = tipo;
             this.nome = nome;
             this.salario = salario;
+
+            LancarRegistrosNoBancoDeDados(this);
+            GerarDocumentosFiscais(this);
+            EnviarEmailDeBoasVindas(this);
+        }
+
+        private void EnviarEmailDeBoasVindas(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private void GerarDocumentosFiscais(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private void LancarRegistrosNoBancoDeDados(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
         }
     }
 }
--- refatoracao/Parte3/Aula3/R51.ReplaceConstructorWithFactoryMethod/depois/Funcionario.cs
+++ refatoracao/Parte3/Aula3/R51.ReplaceConstructorWithFactoryMethod/depois/Funcionario.cs
@@ -8,79 +8,46 @@ namespace refatoracao.R51.ReplaceConstructorWithFactoryMethod.depois
     {
         void Main()
         {
-            Funcionario engenheiro = Funcionario.CriarFuncionario(Funcionario.TipoFuncionario.Engenheiro, "José da Silva", 1000);
-            Funcionario vendedor = Funcionario.CriarFuncionario(Funcionario.TipoFuncionario.Vendedor, "Maria Bonita", 2000);
-            Funcionario gerente = Funcionario.CriarFuncionario(Funcionario.TipoFuncionario.Gerente, "João das Neves", 3000);
-
-            var valorFolhaDePagamento =
-                engenheiro.Salario
-                + vendedor.Salario
-                + gerente.Salario;
+            Funcionario funcionario = Funcionario.CriarFuncionario("José da Silva", 1000);
         }
     }
 
     class Funcionario
     {
-        public enum TipoFuncionario
-        {
-            Engenheiro = 0,
-            Vendedor = 1,
-            Gerente = 2
-        }
-
-        protected TipoFuncionario tipo;
-        public TipoFuncionario Tipo { get; }
-
-        protected string nome;
+        readonly string nome;
         public string Nome => nome;
 
-        protected decimal salario;
+        readonly decimal salario;
         public decimal Salario => salario;
 
-        protected Funcionario(string nome, decimal salario)
+        private Funcionario(string nome, decimal salario)
         {
             this.nome = nome;
             this.salario = salario;
         }
 
-        public static Funcionario CriarFuncionario(TipoFuncionario tipo, string nome, decimal salario)
+        public static Funcionario CriarFuncionario(string nome, decimal salario)
         {
-            switch (tipo)
-            {
-                case TipoFuncionario.Engenheiro:
-                    return new Engenheiro(nome, salario);
-                case TipoFuncionario.Vendedor:
-                    return new Vendedor(nome, salario);
-                case TipoFuncionario.Gerente:
-                    return new Gerente(nome, salario);
-                default:
-                    throw new ArgumentException("Tipo de funcionário inválido");
-            }
+            Funcionario funcionario = new Funcionario(nome, salario);
+            LancarRegistrosNoBancoDeDados(funcionario);
+            GerarDocumentosFiscais(funcionario);
+            EnviarEmailDeBoasVindas(funcionario);
+            return funcionario;
+        }
+
+        private static void EnviarEmailDeBoasVindas(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private static void GerarDocumentosFiscais(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
+        }
+
+        private static void LancarRegistrosNoBancoDeDados(Funcionario funcionario)
+        {
+            //método criado apenas para ilustração
         }
     }
-
-    class Engenheiro : Funcionario
-    {
-        public Engenheiro(string nome, decimal salario) : base(nome, salario)
-        {
-            this.tipo = TipoFuncionario.Engenheiro;
-        }
-    }
-
-    class Vendedor : Funcionario
-    {
-        public Vendedor(string nome, decimal salario) : base(nome, salario)
-        {
-            this.tipo = TipoFuncionario.Vendedor;
-        }
-    }
-
-    class Gerente : Funcionario
-    {
-        public Gerente(string nome, decimal salario) : base(nome, salario)
-        {
-            this.tipo = TipoFuncionario.Gerente;
-        }
-    }
-
 }
